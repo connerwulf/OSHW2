6a7eed19465bd746987692dddf5e5a6e739bd67d
Conner <connerwulf@usf.edu>
2021-02-10 15:46:34 -0500

    dwa
    dd

--- prog1t-ConnerWulf.c
+++ prog1t-ConnerWulf.c
@@ -32,16 +32,19 @@ void* thread1(void *arg)
 {
 	int line = 0;
 	while (line < 2750000)
-	   {
-            line++;
-
-counter->value = counter->value + 1;
+	{
+      line++;
+      if(counter->value % 100 == 0)
+      {
+        counter->value += 100;
+      }
+      counter->value = counter->value + 1;
 	    counter->value = counter->value * 2;
 	    counter->value = counter->value / 2;
 
-           }
+  }
 	printf("from process1 counter  =  %d, mytot %d, jumps %d \n", counter->value, mytot, jumps);
-return(NULL);
+  return(NULL);
 }
 
 
@@ -73,8 +76,8 @@ return(NULL);
 ****************************************************************/
 int main()
 {
-  int  r=0;
-	int 		i;
+  int r=0;
+	int i;
   pthread_t	tid1[1];     /* process id for thread 1 */
   pthread_t	tid2[1];     /* process id for thread 2 */
   pthread_t	tid3[1];     /* process id for thread 2 */
@@ -90,32 +93,30 @@ int main()
 	fflush(stdout);
  /* Required to schedule thread independently.
  Otherwise use NULL in place of attr. */
-        pthread_attr_init(&attr[0]);
-        pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);  /* system-wide contention */
+  pthread_attr_init(&attr[0]);
+  pthread_attr_setscope(&attr[0], PTHREAD_SCOPE_SYSTEM);  /* system-wide contention */
 
 	/* end to schedule thread independently */
 
 /* Create the threads */
 
-        pthread_create(&tid2[0], &attr[0], thread2, NULL);
-        pthread_create(&tid1[0], &attr[0], thread1, NULL);
+  pthread_create(&tid2[0], &attr[0], thread2, NULL);
+  pthread_create(&tid1[0], &attr[0], thread1, NULL);
 
 
 /* Wait for the threads to finish */
-    pthread_join(tid2[0], NULL);
-    pthread_join(tid1[0], NULL);
+  pthread_join(tid2[0], NULL);
+  pthread_join(tid1[0], NULL);
 
 
 
-	        printf("from parent counter  =  %d\n", counter->value);
-    getrusage(RUSAGE_SELF, &mytiming);
-    printf("Time used is sec: %d, usec %d\n",mytiming.ru_utime.tv_sec,
-           mytiming.ru_utime.tv_usec);
-    printf("System Time used is sec: %d, usec %d\n",mytiming.ru_stime.tv_sec,
-           mytiming.ru_stime.tv_usec);
-		printf("---------------------------------------------------------------------------\n");
-		printf("\t\t	End of simulation\n");
+	printf("from parent counter  =  %d\n", counter->value);
+  getrusage(RUSAGE_SELF, &mytiming);
+  printf("Time used is sec: %d, usec %d\n",mytiming.ru_utime.tv_sec,mytiming.ru_utime.tv_usec);
+  printf("System Time used is sec: %d, usec %d\n",mytiming.ru_stime.tv_sec,mytiming.ru_stime.tv_usec);
+	printf("---------------------------------------------------------------------------\n");
+	printf("\t\t	End of simulation\n");
 
-		exit(0);
+	exit(0);
 
 }
