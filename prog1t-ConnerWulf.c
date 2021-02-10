99c9a46a2a72a6dfb2f861964e89952f31c68448
Conner <connerwulf@usf.edu>
2021-02-10 17:21:32 -0500

    awdsadas

--- prog1t-ConnerWulf.c
+++ prog1t-ConnerWulf.c
@@ -34,7 +34,7 @@ void * thread1(void *arg)
 	int line = 0;
 	while (line < 2750000)
 	{
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&counter);
       line++;
       if(counter->value % 100 == 0)
       {
@@ -44,7 +44,7 @@ void * thread1(void *arg)
       counter->value = counter->value + 1;
 	    counter->value = counter->value * 2;
 	    counter->value = counter->value / 2;
-      pthread_mutex_unlock(&mutex);
+      pthread_mutex_unlock(&counter);
   }
 	printf("from process1 counter  =  %d, mytot %d, jumps %d \n", counter->value, mytot, jumps);
   return(NULL);
@@ -65,14 +65,14 @@ void * thread2(void *arg)
     // if (mtx == 0)
     // {
     //
-    pthread_mutex_lock(&mutex);
+    pthread_mutex_lock(&counter);
        line++;
 /* Critical Section */
 	       counter->value = counter->value + 1;
 	       counter->value = counter->value * 2;
 	       counter->value = counter->value / 2;
-         pthread_mutex_unlock(&mutex);
-    
+         pthread_mutex_unlock(&counter);
+
    }
 	   printf("from process2 counter = %d\n", counter->value);
 return(NULL);
